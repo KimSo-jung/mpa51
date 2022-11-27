3b6a0995cd31b06b2a0391f24b66341c17fbed5e
KimSo-jung <mobssy21@gmail.com>
2022-11-27 18:51:49 +0900

    .

---
+++ app/src/main/java/com/example/myapplication/fragments/LoginFragment.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentEmailLoginBinding
+import com.example.myapplication.viewmodels.LoginMainViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    private val loginViewModel:LoginMainViewModel by viewModels()
+    private var bind:FragmentEmailLoginBinding?=null
+    private val binding get() = bind!!
+    private lateinit var auth:FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        bind = FragmentEmailLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        val usernameEditText = binding.username
+        val passwordEditText = binding.password
+        val loginButton = binding.login
+
+        /*
+        val afterTextChangedListener = object : TextWatcher{
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                loginViewModel.loginDataChanged(
+                    usernameEditText.text.toString(),
+                    passwordEditText.text.toString()
+                )
+            }
+        }
+        */
+    }
+}
--- app/src/main/java/com/example/myapplication/viewmodels/LoginMainViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/LoginMainViewModel.kt
@@ -1,8 +1,55 @@
 package com.example.myapplication.viewmodels
 
+import android.net.Uri
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.R
+import com.example.myapplication.nicknameUsable
 
 class LoginMainViewModel : ViewModel() {
+    private val ID_ = MutableLiveData<String>("")
+    private val pw = MutableLiveData<String>("")
+    val id = LiveData<String> = ID_
+    val password: LiveData<String> = pw
 
+    private val loginFormats = MutableLiveData<LoginFormState>()
+    val loginFormState:LiveData<LoginFormState> = loginFormats
+
+    val nickname = MutableLiveData<String>("")
+
+    val uri = MutableLiveData<Uri?>()
+
+    fun setURI(URI:Uri?){
+        if(URI!=null) this.uri.value = URI
+    }
+
+    fun loginDataChanged(username:String, password:String){
+        if(!UserNameValidation(username)){
+            loginFormats.value = LoginFormState(usernameError = R.string.invalid_username)
+        }else if(!PasswordValidation(password)){
+            loginFormats.value = LoginFormState(passwordError = R.string.invalid_password)
+        }else{
+            loginFormats.value = LoginFormState(DataValidation = true)
+        }
+    }
+
+    private fun UserNameValidation(username: String):Boolean{
+        return if(username.contains("@")){
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        }else{
+            username.isNotBlank()
+        }
+    }
+
+    private fun PasswordValidation(password: String):Boolean{
+        return password.length > 6
+    }
 
 }
+data class LoginFormState(
+    val usernameError:Int? = null,
+    val passwordError:Int? = null,
+    val DataValidation:Boolean = false
+)
